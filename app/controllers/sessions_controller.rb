92d0085ba2e59c29cbfd374d381e111e54829b79
Cecile Rougnaux <cecile_rougnaux@hotmail.com>
2021-04-21 18:09:00 +0100

    user and message models added + authentication logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,23 @@ class SessionsController < ApplicationController
   end
 
   def create
-
+    user = User.find_by(username: params[:session][:username].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:notice] = "Logged in successfully"
+      redirect_to root_path
+    else
+      flash.now[:alert] = "There was something wrong with your login details"
+      render 'new'
+    end
   end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = "Logged out"
+    redirect_to root_path
+  end
+
 end
+
+
